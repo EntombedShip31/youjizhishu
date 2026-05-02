87659b96d5cbc6820839aff3cf7bd2b873bdd45e
EntombedShip31 <2802677917@qq.com>
2026-05-02 22:17:26 +0800

    fix port to 8501

--- app.py
+++ app.py
@@ -41,8 +41,5 @@ def settings():
 def static_files(filename):
     return send_from_directory('assets', filename)
 
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', 'False').lower() == 'true')
+    app.run(host="0.0.0.0", port=8501)
